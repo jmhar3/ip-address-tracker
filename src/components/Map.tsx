cae10bc679aef0c25fb3f40ea49c04a58b015e19
Jessica Harriman <jess@perxhealth.com>
2023-08-21 19:20:45 +1000

    add ability to change map view

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,43 +1,51 @@
 import * as React from "react";
 import { Box } from "@chakra-ui/react";
 import { Icon, LatLngTuple, Point } from "leaflet";
-
-import { MapContainer, Marker, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, TileLayer, useMap } from "react-leaflet";
 
 export interface MapProps {
-  location: LatLngTuple;
+  location?: LatLngTuple;
 }
 
 export const Map = (props: MapProps) => {
   const { location } = props;
 
-  const mapState = {
-    center: location,
-    zoom: 12,
-  };
-
-  return (
-    <Box h={{ base: "450px", md: "600px" }} w="100%" bg="gray.100">
+  const displayMap = React.useMemo(
+    () => (
       <MapContainer
+        zoom={12}
+        center={location}
         zoomControl={false}
-        zoom={mapState.zoom}
-        center={mapState.center}
         scrollWheelZoom={false}
         style={{ height: "100%", zIndex: 1 }}
       >
+        {location && <ChangeView location={location} />}
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={location} icon={CustomIcon} />
+        {location && <Marker position={location} />}
       </MapContainer>
+    ),
+    [location]
+  );
+
+  return (
+    <Box h={{ base: "450px", md: "600px" }} w="100%" bg="gray.100">
+      {displayMap}
     </Box>
   );
 };
 
-const CustomIcon = new Icon({
-  iconUrl: require("../images/icon-location.svg"),
-  iconRetinaUrl: require("../images/icon-location.svg"),
-  iconSize: new Point(60, 75),
-  className: "leaflet-div-icon",
-});
+// const CustomIcon = new Icon({
+//   iconUrl: require("../images/icon-location.svg"),
+//   iconRetinaUrl: require("../images/icon-location.svg"),
+//   iconSize: new Point(60, 75),
+//   className: "leaflet-div-icon",
+// });
+
+const ChangeView = (props: { location: LatLngTuple }) => {
+  const map = useMap();
+  map.setView(props.location);
+  return null;
+};
